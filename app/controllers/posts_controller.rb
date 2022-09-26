1114a174a89e169a52858bbf9ab5f3eccf100a98
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-26 23:21:25 +0300

    commit tree b5a2740e9290b9735dede3e480726096750acd6a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -9,9 +11,11 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -19,5 +23,14 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+
+  # def render_not_found_response
+  #   render json: { error: "Post not found" }, status: :not_found
+  # end
+
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    
+  # end
 
 end
